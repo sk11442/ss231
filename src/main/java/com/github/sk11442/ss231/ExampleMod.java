866b6fb4806cf0931e8de49dcdf157d8a1edbccc
GitHub Action <action@github.com>
2025-04-17 21:57:31 +0000

    Initialize template repository proper
    Set package name to com.github.sk11442.ss231
    Set project name to ss231
    Set modid to ss

--- src/main/java/com/github/sk11442/ss231/ExampleMod.java
+++ src/main/java/com/github/sk11442/ss231/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.sk11442.ss231;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "ss", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/sk11442/ss231/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/sk11442/ss231/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.sk11442.ss231.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
